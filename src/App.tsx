064040a7799fbfd266c0c33c6039fe3d90405b87
Leonardo Iglesias <laiglesias.min@gmail.com>
2025-10-10 18:52:56 -0500

    fix: Reformat code style in App.tsx for improved readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import styles from './App.module.css';
-import { SkillTreeLoader } from './Components/SkillTree/skill-tree-loader';
-import data from './data.json';
+import styles from "./App.module.css";
+import { SkillTreeLoader } from "./Components/SkillTree/skill-tree-loader";
+import data from "./data.json";
 
 function App() {
-  return (
-    <div className={styles.app}>
-      <div className={styles.border}>
-        <div className={styles.shadow}>
-          <h1 className={styles.title}>Minecraft</h1>
-          <div className={styles.container}>
-            <SkillTreeLoader data={data} />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.app}>
+			<div className={styles.border}>
+				<div className={styles.shadow}>
+					<h1 className={styles.title}>Minecraft</h1>
+					{/** biome-ignore lint/correctness/useUniqueElementIds: <explanation> */}
+					<div className={styles.container} id="root-container-skill-tree">
+						<SkillTreeLoader data={data} />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
